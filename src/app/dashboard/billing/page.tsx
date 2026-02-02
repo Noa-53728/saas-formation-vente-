3df3c4ddf2826930afab75717b41cd2a67a8337e
Noa <noa.beudot@gmail.com>
2026-02-02 19:06:58 +0100

    add plans and subscriptions base

---
+++ src/app/dashboard/billing/page.tsx
@@ -0,0 +1,10 @@
+import SubscribeButtons from "./SubscribeButtons";
+
+export default function BillingPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Abonnement</h1>
+      <SubscribeButtons />
+    </div>
+  );
+}
